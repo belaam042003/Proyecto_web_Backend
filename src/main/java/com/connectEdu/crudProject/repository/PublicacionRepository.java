07231552e33df3007dc918d82ede4f5f98846784
Guayixxx <111261611+Guayixxx@users.noreply.github.com>
2023-09-22 21:08:42 -0500

    se elimina una carpeta que siempre aparece

--- src/main/java/com/connectEdu/crudProject/repository/PublicacionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.connectEdu.crudProject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.connectEdu.crudProject.model.Publicacion;
-
-public interface PublicacionRepository extends JpaRepository<Publicacion, Long>{
-    
-}
