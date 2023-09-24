9b5e1fcf3ab13203fd5d157180786fd5655de63e
Guayixxx <111261611+Guayixxx@users.noreply.github.com>
2023-09-23 22:42:16 -0500

    Cree la primera capa de seguridad

---
+++ src/main/java/com/connectEdu/crud/Demo/DemoController.java
@@ -0,0 +1,16 @@
+package com.connectEdu.crud.Demo;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class DemoController {
+    
+    @PostMapping(value = "demo")
+    public String welcome(){
+        return "Welcome to secure endpoint";
+    }
+
+}
